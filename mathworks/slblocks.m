39ce0609f0328639e7eae4e612ce7a2b3e5c17cf
sabrin99m <s.maatoug@studenti.unibg.it>
2025-10-30 01:31:53 +0100

    cleaningfolders

--- mathworks/slblocks.m
+++
@@ -1,8 +0,0 @@
-function blkStruct = slblocks
-    blkStruct.Name = 'Effects Library'; 
-    blkStruct.OpenFcn = 'mweffectlib'; 
-    blkStruct.MaskDisplay = 'disp(''mweffectlib'')';
-
-    % Aggiungi automaticamente la cartella delle funzioni al percorso
-    addpath(genpath(fullfile(fileparts(mfilename('fullpath')), 'functions')));
-end
